ed890211021e9df1f9b97331c3ffeaa562bd9e46
krzers <tyhffggfg@gmail.com>
2024-08-31 18:33:20 +0700

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="660610999" />
+      <Footer year="2024" fullName="Natrada Nuchit" studentId="660610757" />
     </div>
   );
 }
